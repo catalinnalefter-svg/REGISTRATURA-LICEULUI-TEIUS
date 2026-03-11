f2d077025f98ee29bb831285b631355c28973027
catalinnalefter-svg <catalinna.lefter@gmail.com>
2026-03-11 10:56:04 +0200

    Update RegistruClient.jsx

--- app/RegistruClient.jsx
+++ app/RegistruClient.jsx
@@ -269,8 +269,8 @@ export default function RegistruTeius() {
     >
                     {activeTab === 'general' ? (
                       <>
-                        <td className="px-4 py-4"><span className={`px-3 py-1.5 rounded-xl text-[9px] font-black text-white ${item.tip === 'INTRARE' ? 'bg-blue-600' : 
-    item.tip === 'IESIRE' ? 'bg-emerald-500' : 
+                        <td className="px-4 py-4"><span className={`px-3 py-1.5 rounded-xl text-[9px] font-black text-white ${item.tip === 'INTRARE' ? 'bg-emerald-500' : 
+    item.tip === 'IESIRE' ? 'bg-blue-600' : 
     'bg-orange-500'}`}>{item.tip}</span></td>
                         <td className="px-4 py-4 text-blue-600 font-black">{item.numar_inregistrare}</td>
                         <td className="px-4 py-4">{item.creat_la}</td>
